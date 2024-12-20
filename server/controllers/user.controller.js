8e589bb01a5c10287c164db68a18ced5c845479a
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-12-20 22:17:04 +0530

    complete register and login controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,8 @@ import sendEmail from '../config/sendEmail.js';
 import UserModel from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import verifyEmailTemplate from '../utils/verifyEmailTemplate.js'; // Adjust path as needed
+import generatedAccessToken from '../utils/generatedAccessToken.js';
+import generatedRefreshToken from '../utils/generatedRefreshToken.js';
 
 const { hash } = bcryptjs;
 
@@ -74,4 +76,104 @@ export async function registerUserController(request, response) {
             success: false
         });
     }
+}
+
+export async function verifyEmailController(request, response) {
+    try {
+        const { code } = request.body
+        const user = await UserModel.findOne({ _id: code });
+       
+        if(!user) {
+            return response.status(400).json({
+                message: "Invalid Code",
+                error: true,
+                success: false
+            });
+        }
+
+        const updateUser = await UserModel.updateOne({ _id: code }, { verify_email: true });
+        
+        return response.json({
+            message: "Email verified successfully",
+            error: false,
+            success: true,
+        });
+
+    } catch (error) {
+        return response.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: true
+        })
+    }
+}
+
+
+export async function loginController(request, response) {
+     try {
+         const { email, password } = request.body
+
+         if (!email || !password) {
+             return response.status(400).json({
+                 message: "Provide email and password",
+                 error: true,
+                 success : false
+             })
+         }
+         
+         const user = await UserModel.findOne({ email })
+
+         if (!user) {
+             return response.status(400).json({
+                    message: "Invalid email or password",
+                    error: true,
+                    success: false
+             })
+         }
+
+         if (user.status !== "Active") {
+             return response.status(400).json({
+                    message: "Contact Admin",
+                    error: true,
+                    success: false
+               })
+}
+         const checkPassword = await bcryptjs.compare(password, user.password)
+         
+         if (!checkPassword) {
+             return response.status(400).json({
+                    message: "Invalid password, check and try again",
+                    error: true,
+                    success: false
+             })
+         }
+
+         const accessToken = await generatedAccessToken(user._id)
+         const refreshToken = await generatedRefreshToken(user._id)
+
+         const cookiesOption = {
+             httpOnly: true,
+             secure: true,
+             sameSite: 'None'
+         }
+         response.cookie('accessToken', accessToken, cookiesOption)
+         response.cookie('refreshToken', refreshToken, cookiesOption)
+
+         return response.json({
+             message: "Login Successfully",
+             error: false,
+             success: true,
+             data: {
+                 accessToken,
+                 refreshToken
+             }
+        })
+
+     } catch (error) {
+         return response.status(500).json({
+             message: error.message || error,
+             error: true,
+            success: false
+        })
+     }
 }
